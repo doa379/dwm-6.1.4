afaadabc72271887b126f3a3ae5f84779f1ded8a
doa379 <doa379@gmail.com>
2015-11-29 00:23:03 +0000

    First Commit

--- transient.c
+++ transient.c
@@ -5,15 +5,18 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-int main(void) {
+int main(void)
+{
 	Display *d;
 	Window r, f, t = None;
 	XSizeHints h;
 	XEvent e;
 
 	d = XOpenDisplay(NULL);
+
 	if (!d)
 		exit(1);
+
 	r = DefaultRootWindow(d);
 
 	f = XCreateSimpleWindow(d, r, 100, 100, 400, 400, 0, 0, 0);
@@ -24,10 +27,13 @@ int main(void) {
 	XMapWindow(d, f);
 
 	XSelectInput(d, f, ExposureMask);
-	while (1) {
+
+	while (1)
+	{
 		XNextEvent(d, &e);
 
-		if (t == None) {
+		if (t == None)
+		{
 			sleep(5);
 			t = XCreateSimpleWindow(d, r, 50, 50, 100, 100, 0, 0, 0);
 			XSetTransientForHint(d, t, f);
